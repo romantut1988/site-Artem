4c241d6d7123e353f6abd9d8ee680427c54e9e62
Roman <romantut1988@mail.ru>
2023-10-02 19:47:19 +0300

    header content adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active')
+}
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     pagination: {
