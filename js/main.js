427379f30618529cca4db604935cf61df0f35ab6
Roman <romantut1988@mail.ru>
2023-11-13 23:21:38 +0300

    changes to class names

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = () => {
-    navbar.classList.remove('active')
-}
-
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
